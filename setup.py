bb6d6468581f6b1bd3ea25be241f1f421137fd5f
John Wason <wason@wasontech.com>
2022-12-31 03:32:11 -0500

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,25 +1,12 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
-    name='pyri-robotics-motion-program',
-    version='0.1.0',
-    description='PyRI Teach Pendant Robotics Motion Program Package',
-    author='John Wason',
-    author_email='wason@wasontech.com',
-    url='http://pyri.tech',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'pyri-common',
-        'pyri-robotics',
-        'robot-motion-program-opt[robotraconteur]'
-    ],
-    entry_points = {
-        'pyri.plugins.sandbox_functions': ['pyri-robotics-sandbox-functions=pyri.robotics_motion_program.sandbox_functions:get_sandbox_functions_factory'],
-        'pyri.plugins.blockly': ['pyri-robotics-plugin-blockly=pyri.robotics_motion_program.blockly:get_blockly_factory'],
-        'pyri.plugins.webui_server': ['pyri-robotics-motion-program-webui-server=pyri.robotics_motion_program.webui_server:get_webui_factory'],
-        'pyri.plugins.robdef': ['pyri-robotics-motion-program-robdef=pyri.robotics_motion_program.robdef:get_robdef_factory'],
+    package_data = {
+        'pyri.robotics_motion_program': ['*.robdef','*.js','*.yml'],
+        'pyri.robotics_motion_program.webui_static': ['*.robdef','*.js','*.yml']
     }
 )
